46c727ff5611eaf3fcaa44f35941f7f4e483c9cf
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2023-02-23 22:22:29 -0300

    moduleExportsTeste

---
+++ node/moduloCliente.js
@@ -0,0 +1,9 @@
+const moduloA = require('./moduloA.js')
+const moduloB = require('./moduloB.js')
+
+
+console.log(moduloA)
+console.log(moduloB)
+
+console.log(moduloA.ateLogo)
+console.log(moduloB.boaNoite())
