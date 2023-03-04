1031adeda5e47914da1d2b0f3ed79c9dbefc636a
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2023-03-04 20:39:24 -0300

    firstProjectWithExpress

---
+++ node/temporizador.js
@@ -0,0 +1,22 @@
+const schedule = require('node-schedule')
+
+const regra = new schedule.RecurrenceRule()
+regra.dayOfWeek = [new schedule.Range(1, 6)]
+regra.hour = 20
+regra.second = 30
+
+
+const tarefa1 = schedule.scheduleJob(regra, function(){
+    console.log('Executando Tarefa 1', new Date().getSeconds())
+})
+
+setTimeout(function(){
+    tarefa1.cancel()
+    console.log('Cancelando Tarefa 1!')
+}, 61000)
+
+
+
+const tarefa2 = schedule.scheduleJob(regra, function(){
+    console.log('Executando Tarefa 2', new Date().getSeconds())
+})
